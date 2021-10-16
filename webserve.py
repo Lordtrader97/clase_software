589314f466b8073a5c6130d74d3354752cf4d341
Harry Hernandez <poncioharry@gmail.com>
2021-10-15 21:09:52 -0500

    endpoints de eliminar, agregar y consultar

--- webserve.py
+++ webserve.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import numpy as np
 
 app = Flask(__name__)
 
@@ -18,6 +19,8 @@ def hello_world():
 
   
 f=0
+nombre=[0,0,0,0,0]
+apellido=[0,0,0,0]
 
 @app.route("/form")
 def form():
@@ -25,21 +28,48 @@ def form():
 
 @app.route("/clientes",methods=['get'])
 def clientes():
-	return{"clientes":"Devolviendo todos los clientes"}	
+	global nombre
+	return {"Clientes":nombre}	
 
 @app.route("/clientes/<id>",methods=['get'])
+
 def cliente(id):
-	return{"cliente":{"id":id,"Nombre":"NN"}}
+	global nombre
+	return{"cliente":{"id":int(id),"Nombre":nombre[int(id)]}}
 
 @app.route("/register",methods=['post'])
 def crear_cliente():
 	global f
-	nombre= request.form.get("nombre")
-	apellido=request.form.get("apellido")
+	global nombre
+	global apellido
+	nombre[f]=(request.form.get("nombre"))
+	apellido[f]=(request.form.get("apellido"))
 	print("Creando cliente...")
 	f=f+1
-	return {"cliente":{"id":f,"nombre":nombre,"apellido":apellido}}
+	return {"cliente":{"id":f,"nombre":nombre[f-1],"apellido":apellido[f-1]}}
 
+
+@app.route("/cliente/editar/<id>",methods=['put'])
+def editar_cliente(id):
+	global nombre
+	nombre[int(id)]=(request.form.get("nombre"))
+	apellido[int(id)]=(request.form.get("apellido"))
+	print("Creando cliente...")
+	return {"editando cliente ..":{"id":f,"nombre":nombre[int(id)],"apellido":apellido[int(id)]}}
+
+@app.route("/cliente/editar/nombre/<id>",methods=['put'])
+def editar_cliente_nombre(id):
+	global nombre
+	nombre[id]=(request.form.get("nombre"))
+	print("Editando cliente...")
+	return {"cliente":{"id":f,"nombre":nombre[id]}}
+
+@app.route("/cliente/borrar/<id>",methods=['delete'])
+def eliminar_cliente(id):
+	global nombre
+	nombre[int(id)]= "sin info"
+	apellido[int(id)]= "sin info"
+	return {"Cliente eliminado":"ok"}
 
 if __name__=='__main__':
 	app.run()
