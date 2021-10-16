e1e0b567b4c7b90689e6934e3c02d29d4ebb5b1a
Harry Hernandez <poncioharry@gmail.com>
2021-10-15 19:57:42 -0500

    microservicio para obtener los clientes

--- webserve.py
+++ webserve.py
@@ -9,14 +9,28 @@ def hello_world():
 	#nombre=request.args.post("nombre")
 	#edad=request.args.get("edad")
 	nombre=request.form.get("nombre")
-	edad=request.form.get("edad")
-	return f"<h1> Bienvenido {nombre}</h1><h2>Usted tiene {edad}</h2>"
+	edad=int(request.form.get("edad"))
+	if edad >= 18:
+		aceptado = True
+	else:
+		aceptado = False
+	return{"aceptado":aceptado}
+
   
 
 
 @app.route("/form")
 def form():
 	return render_template("form.html")
+
+@app.route("/clientes",methods=['get'])
+def clientes():
+	return{"clientes":"Devolviendo todos los clientes"}	
+
+@app.route("/clientes/<id>",methods=['get'])
+def cliente(id):
+	return{"cliente":{"id":id,"Nombre":"NN"}}
+
 if __name__=='__main__':
 	app.run()
 
